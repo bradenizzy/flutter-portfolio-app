5e3ed6388aded2da993046765447cb0d7e546dff
bradenizzy <braden.israelsen@gmail.com>
2024-11-14 13:47:30 -0700

    service file

--- lib/blackjack/services/a_service.dart
+++ lib/blackjack/services/a_service.dart
@@ -1,1 +1,7 @@
 // a_service.dart
+
+// this is a placeholder to show how I would create a seperate folder for service files
+// I would then import the service file into the provider file
+// I would then use the service file to fetch data from the internet or local database
+// I would then return the data to the provider file
+// The provider file would then update the state of the app
