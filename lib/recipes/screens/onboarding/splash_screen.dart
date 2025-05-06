e0bd742822b28f24ce68bc67589f28cdf47cfa70
bradenizzy <braden.israelsen@gmail.com>
2025-05-06 19:38:05 -0400

    onboarding screens

---
+++ lib/recipes/screens/onboarding/splash_screen.dart
@@ -0,0 +1,35 @@
+// splash_screen.dart
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigate();
+  }
+
+  Future<void> _navigate() async {
+    final prefs = await SharedPreferences.getInstance();
+    final seenOnboarding = prefs.getBool('hasSeenOnboarding') ?? false;
+
+    if (seenOnboarding) {
+      Navigator.of(context).pushReplacementNamed('/home');
+    } else {
+      Navigator.of(context).pushReplacementNamed('/onboarding');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
